8b4b0e0ce3edbaf65bf2e116899438e7a5c1a04d
trevormil2001 <trevormil2001@vt.edu>
2022-02-21 17:26:36 -0500

    admin auth plus npm audit

--- functions/utils/AdminAuth.js
+++ functions/utils/AdminAuth.js
@@ -1,7 +1,7 @@
-const { admin, db } = require("./admin");
+const { admin, db } = require('./admin');
 
 module.exports = (req, res, next) => {
-  if (req.user.userName === "TMAdmin") {
-    return next();
-  } else return res.status(403).json({ admin: "Admin privileges only!" });
+    if (req.user.userName === 'trevormil') {
+        return next();
+    } else return res.status(403).json({ admin: 'Admin privileges only!' });
 };
