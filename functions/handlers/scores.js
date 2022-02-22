2f9ad7ccea4acdf6a569bb0eb6004a2ef9f5966d
trevormil2001 <trevormil2001@vt.edu>
2022-02-21 19:31:03 -0500

    change get scores from fetch() to axios()

--- functions/handlers/scores.js
+++ functions/handlers/scores.js
@@ -1,32 +1,36 @@
+const axios = require('axios').default;
+
 //gets all stocks
 exports.getAllScores = (req, res) => {
-  const url =
-    "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard";
-  fetch(url, {
-    method: "get",
-  })
-    .then((data) => data.json())
-    .then((response) => {
-      let scores = [];
+    const url =
+        'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
+    axios
+        .get(url)
+        .then((res) => {
+            return res.data;
+        })
+        .then((response) => {
+            console.log(response);
+            let scores = [];
 
-      response["events"].forEach((element) => {
-        let gameInfo = {};
-        gameInfo.name = element["name"];
-        gameInfo.shortName = element["shortName"];
-        gameInfo.score = [];
-        element["competitions"].forEach((elem) => {
-          elem["competitors"].forEach((e) => {
-            gameInfo.score.push({
-              score: e["score"],
-              logo: e["team"]["logo"],
+            response['events'].forEach((element) => {
+                let gameInfo = {};
+                gameInfo.name = element['name'];
+                gameInfo.shortName = element['shortName'];
+                gameInfo.score = [];
+                element['competitions'].forEach((elem) => {
+                    elem['competitors'].forEach((e) => {
+                        gameInfo.score.push({
+                            score: e['score'],
+                            logo: e['team']['logo'],
+                        });
+                    });
+                });
+                scores.push(gameInfo);
             });
-          });
+            return res.status(201).json(scores);
+        })
+        .catch((err) => {
+            console.log(err);
         });
-        scores.push(gameInfo);
-      });
-      return res.status(201).json(scores);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 };
