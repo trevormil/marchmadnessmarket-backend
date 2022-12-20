c6115e6756fc5e6e0f847bea1732d2e896ea5d3f
trevormil <trevormil@comcast.net>
2022-12-20 14:42:26 -0500

    dependency updates

--- functions/utils/config.js
+++ functions/utils/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: "AIzaSyC_BOgoYRs7r4Yrfu2E4PfIF_EpOOOjtpw",
-  authDomain: "tm-market.firebaseapp.com",
-  databaseURL: "https://tm-market.firebaseio.com",
-  projectId: "tm-market",
-  storageBucket: "tm-market.appspot.com",
-  messagingSenderId: "1037563924628",
-  appId: "1:1037563924628:web:5093b1012eadc5e420a481",
-  measurementId: "G-SRG37KFM6N",
+    apiKey: 'AIzaSyC_BOgoYRs7r4Yrfu2E4PfIF_EpOOOjtpw',
+    authDomain: 'tm-market.firebaseapp.com',
+    databaseURL: 'https://tm-market.firebaseio.com',
+    projectId: 'tm-market',
+    storageBucket: 'tm-market.appspot.com',
+    messagingSenderId: '1037563924628',
+    appId: '1:1037563924628:web:5093b1012eadc5e420a481',
+    measurementId: 'G-SRG37KFM6N',
 };
